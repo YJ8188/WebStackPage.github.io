a3f0bcecb382ab2ca7752a8c893a4495b8999fe4
小小程序猿 <53797750+YJ8188@users.noreply.github.com>
2026-01-03 16:18:18 +0800

    Update sockets.js

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,1 +1,26 @@
 
+// Utilss.js
+var Utilss = {
+
+  getUUID: function () {
+    return 'xxxxxx'.replace(/[x]/g, function () {
+      return Math.floor(Math.random() * 16).toString(16);
+    });
+  },
+
+  // ⚠️ 关键在这里
+  getWsURL: function () {
+    var ip = Plaintext.ipStr();
+
+    // ⚠️ 端口和路径下一步抓
+    return "ws://" + ip + ":端口/路径";
+  },
+
+  getSendData: function (uid) {
+    return JSON.stringify({
+      type: "ping",
+      time: Date.now()
+    });
+  }
+
+};
